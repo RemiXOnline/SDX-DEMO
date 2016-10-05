945dc387ea3c3252fbe4287fd83fda1bacadcd20
Remi De Jouvencel <rdejouvencel@rdejouvenc-ltm.internal.salesforce.com>
2016-10-05 09:35:05 +0200

    v2

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'html'});
-    response.end('<H1> Hello World </H1>');
+    response.end('<H1> Hello Cloud Elite </H1>');
 }).listen(8080);
 
 console.log('Server started');
